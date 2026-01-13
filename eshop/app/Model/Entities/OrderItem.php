6adbc2f5f540639ca2d02ff3e614c1bd728d134a
seyvv <verca.seyfriedova@gmail.com>
2026-01-13 15:12:00 +0100

    základní verze objednávek v adminu

---
+++ eshop/app/Model/Entities/OrderItem.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Model\Entities;
+
+use LeanMapper\Entity;
+
+/**
+ * Class OrderItem
+ * @package App\Model\Entities
+ *
+ * @property int $orderItemId
+ * @property Order $order m:hasOne
+ * @property Product $product m:hasOne
+ * @property string $productTitle
+ * @property float $unitPrice
+ * @property int $count
+ * @property string $color
+ * @property string $size
+ * @property float $lineTotal
+ */
+
+class OrderItem extends Entity
+{
+
+}
